60a416dbbef2146681ac01a2bde9275d150e8849
slowdive14 <xinlisupreme@naver.com>
2026-04-16 10:20:51 +0900

    chore: add debug banner

--- youtube-digest-archive/.vercel/output/_functions/entry.mjs
+++ youtube-digest-archive/.vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_DKKWKRHR.mjs';
-import { manifest } from './manifest_8g2FX0MK.mjs';
+import { manifest } from './manifest_ZSvAJiek.mjs';
 
 const serverIslandMap = new Map();;
 
@@ -27,7 +27,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "7f94022a-2e9c-4b5c-be6b-07c51bf29840",
+    "middlewareSecret": "83014c9d-d86f-43c7-bac6-536beac2573c",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
